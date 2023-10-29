9ffa53201bf9a5106a29233711e0089ae7081b56
Vinayak Thakur <142525931+Cobrachainsaw@users.noreply.github.com>
2023-10-30 01:08:10 +0530

    updated code

--- includes/functionstore.inc.php
+++ includes/functionstore.inc.php
@@ -1,7 +1,7 @@
 <?php
-    function emptyInputSignup($name, $regnum, $pwd, $phno){
+    function emptyInputSignup($name, $regnum, $pwd){
         $result = true;
-        if(empty($name) || empty($regnum) || empty($pwd) || empty($phno)){
+        if(empty($name) || empty($regnum) || empty($pwd)){
             $result = true;
     }
     else{
@@ -10,10 +10,10 @@
     return $result;
 }
 function uidExists($conn ,$regnum){
-    $sql = "SELECT * FROM users WHERE usersId = ?;";
+    $sql = "SELECT * FROM stores WHERE storesId = ?;";
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql)){
-        header("location: ../RegisterStudent.html?error=stmtfail");
+        header("location: ../RegisterStore.html?error=stmtfail");
         exit();
     }
     mysqli_stmt_bind_param($stmt, 's', $regnum);
@@ -27,18 +27,18 @@ function uidExists($conn ,$regnum){
         return $result;
     }
 }
-function createUser($conn, $name, $regnum, $pwd, $phno){
-    $sql = "INSERT INTO users (usersName, usersRegnum, usersPwd, usersNo) VALUES (?,?,?,?);";
+function createUser($conn, $name, $regnum, $pwd){
+    $sql = "INSERT INTO stores (storesName, storesId, storesPwd) VALUES (?,?,?);";
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql)){
-        header("location: ../RegisterStudent.html?error=stmtfailed");
+        header("location: ../RegisterStore.html?error=stmtfailed");
         exit();
     }
 
     $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-    mysqli_stmt_bind_param($stmt, "ssss", $name, $regnum, $hashedPwd, $phno);
+    mysqli_stmt_bind_param($stmt, "sss", $name, $regnum, $hashedPwd);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
-    header("location: ../RegisterStudent.html?error=none");
+    header("location: ../RegisterStore.html?error=none");
     exit();
 }
--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,24 +1,21 @@
 <?php
-    if(isset($_POST["Register"])){
+    if(isset($_POST["submit"])){
 
         $name = $_POST["fullname"];
         $regnum = $_POST["regnum"];
         $pwd = $_POST["password"];
+        $phno = $_POST["phno"];
 
         require_once 'dbh.inc.php';
         require_once 'functions.inc.php';
 
-        if (emptyInputSignup($name, $regnum, $pwd) !== false){
-            header("location : ../RegisterStudent.html?error = emptyinput");
+        if (emptyInputSignup($name, $regnum, $pwd, $phno) !== false){
+            header("location: ../RegisterStudent.html?error = emptyinput");
             exit();
-
-    }
-    if (uidExists($conn, $regnum) !== false){
-        header("location: ../RegisterStudent.html?error=regnumberexists");
-    } 
-    else{
-        header("location : ../RegisterStudent.html");
-        exit();
-    }
-    createUser($conn, $name, $regnum, $pwd);
+        }
+        if (uidExists($conn, $regnum) !== false){
+            header("location: ../RegisterStudent.html?error=regnumberexists");
+        } 
+    createUser($conn, $name, $regnum, $pwd, $phno);
+    exit();
 }
